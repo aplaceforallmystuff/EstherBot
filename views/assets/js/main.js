36f09b5221cb32ccf8c2ef777dca609078b2e3e7
aplaceforallmystuff <admin@jimchristian.net>
2016-05-02 10:37:54 +0100

    Web stuff

---
+++ views/assets/js/main.js
@@ -0,0 +1,1 @@
+$(function(){$('.mcon').particleground({dotColor:'#DDDDDD',lineColor:'rgba(255, 255, 255, .2)',particleRadius:4,proximity:75,minRadiusPct:.5,maxRadiusPct:2.2,minOpacity:.5,maxOpacity:.8});});
